534b1e5783d92deeb6352ee1b64bbae9e78d772b
shewaixin323 <lifeng9529@gmail.com>
2026-09-16 21:51:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 VERIFY_TOKEN = os.environ.get("VERIFY_TOKEN")
 WHATSAPP_TOKEN = os.environ.get("WHATSAPP_TOKEN")
 PHONE_NUMBER_ID = os.environ.get("PHONE_NUMBER_ID")
-
+print("VERIFY_TOKEN:", VERIFY_TOKEN)
 
 @app.route("/", methods=["GET"])
 def home():
